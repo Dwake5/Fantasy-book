443e37750640bd699cea02eef1deb21fd5ea26c8
Dwake5 <wakeling101@hotmail.com>
2022-09-30 16:24:34 +0100

    tested and can run the project

--- src/index.js
+++ src/index.js
@@ -3,20 +3,22 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>,
+    <ChakraProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ChakraProvider>
   </React.StrictMode>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
